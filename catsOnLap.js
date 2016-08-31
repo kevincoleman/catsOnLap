fa477d2b5d0b6c78070e01fc63eed49948bb07f8
kevincoleman <kevin.n.coleman@gmail.com>
2016-08-31 14:47:44 -0700

    reduced to simpler format

--- catsOnLap.js
+++ catsOnLap.js
@@ -2,36 +2,21 @@ var cat         = { annoyance: 30 }; // cats are never not annoyed
 var lapContents = [];
 var needToPee   = 0;
 
-var catSettle = (settlingCat) => {
-	lapContents.push(settlingCat);
-  console.log("You have a cat on your lap!");
-  return;
-};
+var catSettle = (cat) => lapContents.push(cat);
 
-var theInevitable = () => {
-	needToPee++;
-  console.log("The bathroom beckons ever louder...");
-  return;
-};
+var theInevitable = () => needToPee++;
 
 var useRestroom = () => {
 	lapContents = [];
 	cat.annoyance++;
   needToPee = 0;
-  console.log("Ahh, much better. Your cat glares at you unforgivingly.");
-  return;
 };
 
 while (lapContents.length < 1 && cat.annoyance < 100) {
   catSettle(cat);
   while (lapContents.length > 0) {
     theInevitable();
-    if (needToPee >= 1) {
-      useRestroom();
-    }
-    if (cat.annoyance >= 100) {
-      console.log("Your cat leaves.");
-      break;
-    }
+    if (needToPee >= 1) { useRestroom(); }
+    if (cat.annoyance >= 100) { break; }
   }
 }
