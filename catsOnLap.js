7c02f3bdb8ac6f541b1bc315f4364bd1ebeae9d1
kevincoleman <kevin.n.coleman@gmail.com>
2016-08-31 09:21:55 -0700

    clean up logs and minor es6 updates

--- catsOnLap.js
+++ catsOnLap.js
@@ -1,35 +1,36 @@
-var cat = {
-	annoyance: 30 // cats are never not annoyed
-};
+var cat         = { annoyance: 30 }; // cats are never not annoyed
 var lapContents = [];
-var needToPee = 0;
+var needToPee   = 0;
 
-var catSettle = function(settlingCat) {
+var catSettle = (settlingCat) => {
 	lapContents.push(settlingCat);
-  console.log(".\n.\n.\nYou have a cat on your lap!");
+  console.log("You have a cat on your lap!");
+  return;
 };
 
-var theInevitable = function() {
+var theInevitable = () => {
 	needToPee++;
   console.log("The bathroom beckons ever louder...");
+  return;
 };
 
-var useRestroom = function() {
+var useRestroom = () => {
 	lapContents = [];
 	cat.annoyance++;
   needToPee = 0;
-  console.log("Ahh, much better.\n\nYour cat glares at you unforgivingly.");
+  console.log("Ahh, much better. Your cat glares at you unforgivingly.");
+  return;
 };
 
 while (lapContents.length < 1 && cat.annoyance < 100) {
   catSettle(cat);
-  while (lapContents.length !== 0) {
+  while (lapContents.length > 0) {
     theInevitable();
     if (needToPee >= 1) {
       useRestroom();
     }
-    if (cat.annoyance === 100) {
-      console.log("\n\n\nYour cat leaves.");
+    if (cat.annoyance >= 100) {
+      console.log("Your cat leaves.");
       break;
     }
   }
